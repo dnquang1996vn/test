b69883a1290ab73c4f6d0965e6e0b67b3434329a
Quang Do <quangdotwentyci>
2020-04-22 14:13:46 +0700

    add ide-helper laravel

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+
+class UserController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function index()
+    {
+        return User::all();
+    }
+
+    //
+}
